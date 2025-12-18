4a19f0a3974721488f6173d5cd447ef6dd411622
Bryan Fernando Dinata <bfernando@student.ciputra.ac.id>
2025-12-18 15:20:20 +0700

    add_locked_icons

--- src/components/pages/registration/EarlyNotEarly.tsx
+++ src/components/pages/registration/EarlyNotEarly.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import Link from 'next/link';
+import { HiLockClosed } from "react-icons/hi2";
+import { HiLockOpen } from "react-icons/hi2";
 export interface EarlyNotEarlyProps {
     title: string;
     price?: string;
@@ -12,7 +14,8 @@ export default function EarlyNotEarly(props: EarlyNotEarlyProps) {
   return (
     <Link href={`${props.isActive ? props.href : '#'}`} className="w-full flex justify-center">
         
-      <div className={`w-[80%] sm:w-full transition-colors duration-300 border-4 border-white shadow-lg flex flex-col items-center py-6 px-4 ${props.isActive ? 'opacity-100 cursor-pointer  bg-gradient-to-r from-[#FF7E5F] to-[#FEB47B] hover:from-[#FEB47B] hover:to-[#FF7E5F]' : 'opacity-75 bg-amber-900'}`}>
+      <div className={`w-[80%] sm:w-full transition-colors duration-300 border-4 border-white shadow-lg flex flex-col items-center py-6 px-4 'opacity-100 cursor-pointer  bg-gradient-to-r from-[#FF7E5F] to-[#FEB47B] hover:from-[#FEB47B] hover:to-[#FF7E5F]`}>
+        {props.isActive ? <HiLockOpen size={50} width={300} height={300}/> : <HiLockClosed size={50} width={300} height={300}/> }
         <h2 className="text-3xl font-impact text-white mb-2">{props.title}</h2>
         {props.price && (
           <p className="text-xl font-futura text-white">
